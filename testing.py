8b78a70cf77c13eb3f283ff0aee0e5e8eeddf569
sanketg10 <sg3184@columbia.edu>
2016-04-26 19:16:13 -0400

    First Commit

---
+++ testing.py
@@ -0,0 +1,27 @@
+#!flask/bin/python
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return 'Welcome to our site' 
+
+@app.route('/about')
+def about():
+  return 'We are here to welcome you. Here is our story:' 
+
+@app.route('/file/<user>')
+def file(user):
+  return "<h1>Welcome to our site! %s </h1>" % user
+
+@app.route('/post/<int:id>')
+def post(id):
+  return "<h1>Post ID %s </h1>" % id 
+
+@app.route("/profile/<name>")
+def profile(name):
+  return render_template("profile.html", name=name)
+
+if __name__== "__main__":
+  app.run(debug=True)
