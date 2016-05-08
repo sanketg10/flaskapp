270402d09109ee55430b963cac3effb332667588
sanketg10 <sg3184@columbia.edu>
2016-05-08 13:45:04 -0400

    nav bar changes

--- testing.py
+++ testing.py
@@ -3,22 +3,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/<user>')
-def index(user=None):
-  return render_template("user.html",user=user)
+# @app.route('/')
+# @app.route('/<user>')
+# def index(user=None):
+#   return render_template("user.html",user=user)
 
-@app.route('/about')
-def about():
-  return 'We are here to welcome you. Here is our story:' 
+# @app.route('/about')
+# def about():
+#   return 'We are here to welcome you. Here is our story:' 
 
-# @app.route('/file/<user>')
-# def file(user):
-#   return "<h1>Welcome to our site! %s </h1>" % user
+# # @app.route('/file/<user>')
+# # def file(user):
+# #   return "<h1>Welcome to our site! %s </h1>" % user
 
-@app.route('/post/<int:id>')
-def post(id):
-  return "<h1>Post ID %s </h1>" % id 
+# @app.route('/post/<int:id>')
+# def post(id):
+#   return "<h1>Post ID %s </h1>" % id 
 
 @app.route("/profile/<name>")
 def profile(name):
